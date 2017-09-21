294b108d65a3cd08a3609b01c484f6d1e36939e3
JaeHyeongHa <32117168+JaeHyeongHa@users.noreply.github.com>
2017-09-21 11:53:35 +0900

    Delete onlyBoard_remove.php

--- views/onlyBoard_remove.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$this->onlyBoard_model->remove('commend', $id);     // 댓글 먼저 삭제
-$this->onlyBoard_model->remove('board', $id);       // 글 삭제
-
-echo "<script>document.location.href='http://localhost/index.php/onlyBoard'</script>";
-
-?>
